9fd3a0d2ae146e021edb4bce45a6de207f21a9e0
Ryan Amos <amos2580@hotmail.co.uk>
2022-03-21 22:55:12 +0000

    Added XML parameters for methods

--- CreditsPlugin/BetManager.cs
+++ CreditsPlugin/BetManager.cs
@@ -24,6 +24,10 @@ public class BetManager
     private readonly Dictionary<long, int> _maxScore = new();
     private readonly Dictionary<int, OpenBetData> _openBets = new();
 
+    /// <summary>
+    /// Prints a list of initalised bets in-game
+    /// </summary>
+    /// <param name="gameEvent">GameEvent</param>
     public void GetOpenBets(GameEvent gameEvent)
     {
         if (_openBets.Count == 0)
@@ -43,6 +47,12 @@ public class BetManager
         }
     }
 
+    /// <summary>
+    /// Gets player's ranked position for given server
+    /// </summary>
+    /// <param name="clientId">Client's ID</param>
+    /// <param name="serverId">Server's ID</param>
+    /// <returns>Their ranked position on the server</returns>
     private async Task<int> GetPlayerRankedPosition(int clientId, long serverId)
     {
         await using var context = _contextFactory.CreateContext(false);
@@ -56,6 +66,11 @@ public class BetManager
         return clientRanking?.Ranking + 1 ?? 0;
     }
 
+    /// <summary>
+    /// Queries database for provided Server ID
+    /// </summary>
+    /// <param name="serverId">Server's ID</param>
+    /// <returns></returns>
     private Expression<Func<EFClientRankingHistory, bool>> GetNewRankingFunc(long? serverId = null)
     {
         return ranking => ranking.ServerId == serverId
@@ -68,6 +83,11 @@ public class BetManager
                           _config.TopPlayersMinPlayTime;
     }
 
+    /// <summary>
+    /// Gets the total amount of ranked players on given server
+    /// </summary>
+    /// <param name="serverId">Server's ID</param>
+    /// <returns>The amount of ranked players</returns>
     private async Task<int> GetTotalRankedPlayers(long serverId)
     {
         await using var context = _contextFactory.CreateContext(false);
@@ -77,6 +97,11 @@ public class BetManager
             .CountAsync();
     }
 
+    /// <summary>
+    /// Whether it's been 2 minutes since map change
+    /// </summary>
+    /// <param name="client">EFClient</param>
+    /// <returns>Boolean. True if it has been more than 2 minutes</returns>
     public async Task<bool> CanBet(EFClient client)
     {
         var clientServerId = await client.CurrentServer.GetIdForServer();
@@ -85,6 +110,11 @@ public class BetManager
         return _mapTime[clientServerId].AddMinutes(2) >= DateTime.UtcNow;
     }
 
+    /// <summary>
+    /// Main logic to instantiate a new bet
+    /// </summary>
+    /// <param name="gameEvent">GameEvent</param>
+    /// <param name="amount">Amount of credits</param>
     public async void OnBetCreated(GameEvent gameEvent, int amount)
     {
         var totalKeys = _openBets.Count;
@@ -115,6 +145,10 @@ public class BetManager
             "Payout is made after map rotation. Disconnecting will void bet.");
     }
 
+    /// <summary>
+    /// Checks if provided GameEvent's EFClient has higher score on their server
+    /// </summary>
+    /// <param name="gameEvent">GameEvent</param>
     public async void OnClientUpdated(GameEvent gameEvent)
     {
         var clientServerId = await gameEvent.Origin.CurrentServer.GetIdForServer();
@@ -125,6 +159,10 @@ public class BetManager
         }
     }
 
+    /// <summary>
+    /// Main logic for map rotation - paying out and removing old bets
+    /// </summary>
+    /// <param name="server">Server</param>
     public async void OnMatchEnd(Server server)
     {
         var serverId = await server.GetIdForServer();
